865adccd770dc4c135cc64e10618b7d05ddbb7ac
Fidel Salazar <fidelsalazar990923@gmail.com>
2023-09-20 08:32:32 -0400

    first commint

--- migrations/2_transaction_deploy.js
+++
@@ -1,7 +0,0 @@
-const Transaction = artifacts.require("Transaction.sol");
-
-module.exports = (deployer, network, accounts) => {
-  const _owner = accounts[0];
-   
-  deployer.deploy(Transaction, _owner);
-}
